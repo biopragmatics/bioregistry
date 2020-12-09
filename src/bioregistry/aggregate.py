75fdcba33ed85fe181e066589a470212df3a58d0
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-12-09 02:14:49 +0100

    :memo: Add ols values to pre-existing entries

--- src/bioregistry/aggregate.py
+++ src/bioregistry/aggregate.py
@@ -15,6 +15,7 @@ import requests_ftp
 from bioregistry.constants import BIOREGISTRY_PATH
 from bioregistry.external.miriam import get_miriam_registry
 from bioregistry.external.obofoundry import get_obofoundry
+from bioregistry.external.ols import get_ols
 
 requests_ftp.monkeypatch_session()
 
@@ -38,6 +39,28 @@ OBO_KEYS = {
     'deprecated',
     'description',
 }
+
+
+def _prepare_ols(ols_entry):
+    ols_id = ols_entry['ontologyId']
+    config = ols_entry['config']
+
+    license_value = config['annotations'].get('license', [None])[0]
+    if license_value in {'Unspecified', 'Unspecified'}:
+        license_value = None
+
+    rv = {
+        'prefix': ols_id,
+        'name': config['title'],
+        'download': config['fileLocation'],
+        'version': config['version'],
+        'version.iri': config['versionIri'],
+        'description': config['description'],
+        'license': license_value,
+    }
+
+    rv = {k: v for k, v in rv.items() if v}
+    return rv
 
 
 def _prepare_obo(obofoundry_entry):
@@ -264,9 +287,38 @@ def update_miriam(registry):
     return registry
 
 
+@updater
+def update_ols(registry):
+    ols_id_to_bioregistry_id = {
+        entry['ols']['prefix']: key
+        for key, entry in registry.items()
+        if 'ols' in entry
+    }
+
+    ols_registry = get_ols(mappify=True)
+
+    ols_norm_prefix_to_prefix = {
+        _norm(obo_key): obo_key
+        for obo_key in ols_registry
+    }
+    for bioregistry_id, entry in registry.items():
+        if 'ols' in entry:
+            continue
+        ols_id = ols_norm_prefix_to_prefix.get(_norm(bioregistry_id))
+        if ols_id is not None:
+            entry['ols'] = {'prefix': ols_id}
+            ols_id_to_bioregistry_id[ols_id] = bioregistry_id
+
+    for ols_prefix, ols_entry in ols_registry.items():
+        bioregistry_id = ols_id_to_bioregistry_id.get(ols_prefix)
+        if bioregistry_id is None:
+            continue
+        registry[bioregistry_id]['ols'] = _prepare_ols(ols_entry)
+
+
 @click.command()
 def main():
-    update_obofoundry()
+    update_ols()
 
 
 if __name__ == '__main__':
