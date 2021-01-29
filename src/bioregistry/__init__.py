34d4f97a009ca851605a0817766c6fd3b9c3d9d8
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-01-29 15:42:27 +0100

    Update top level import

--- src/bioregistry/__init__.py
+++ src/bioregistry/__init__.py
@@ -3,7 +3,7 @@
 """Extract registry information."""
 
 from .resolve import (  # noqa
-    get, get_example, get_format, get_name, get_pattern, get_version, get_versions, is_deprecated, normalize_prefix,
-    validate,
+    get, get_example, get_format, get_name, get_pattern, get_pattern_re, get_version, get_versions, is_deprecated,
+    normalize_prefix, validate,
 )
 from .utils import read_bioregistry  # noqa
