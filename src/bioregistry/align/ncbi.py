3150776f8a380138cfffcdfd449cc68882c100d3
Scott Colby <scott@scolby.com>
2021-03-14 00:28:42 -0400

    Docstrings, lint, black.

--- src/bioregistry/align/ncbi.py
+++ src/bioregistry/align/ncbi.py
@@ -11,16 +11,30 @@ __all__ = ["NcbiAligner"]
 
 
 class NcbiAligner(Aligner):
+    """Aligner for NCBI xref registry."""
+
     key = "ncbi"
     getter = get_ncbi
     curation_header = ("name", "generic_urls", "example")
 
     def prepare_external(
-        self, external_id: str, external_entry: Dict[str, str]
+        self,
+        external_id: str,
+        external_entry: Dict[str, str],
     ) -> Dict[str, Any]:
+        """Stub for preparing NCBI xref data to be added to the bioregistry.
+
+        The NCBI getter does all the necessary processing, so this method is effectively an identity.
+
+        :param external_id: the key of the entry in the incoming data
+        :param external_entry: the actual entry data from the incoming data
+
+        :returns: the processed entry data (in this case, no actual processing is done)
+        """
         return external_entry
 
     def get_curation_row(self, external_id, external_entry) -> List[str]:
+        """Return the relevant fields from an NCBI entry for pretty-printing."""
         return [
             external_entry["name"],
             external_entry.get("generic_urls"),
