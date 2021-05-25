235972a1cd59d9d4faeaf216f73bd1a140982d54
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-05-24 20:24:32 -0400

    Update prefix_maps.py

--- src/bioregistry/prefix_maps.py
+++ src/bioregistry/prefix_maps.py
@@ -14,42 +14,25 @@ from bioregistry.constants import DOCS_DATA
 
 @click.command()
 def main():
+    """Generate various JSON-LD context files."""
     contexts_directory = Path(DOCS_DATA) / 'contexts'
     contexts_directory.mkdir(parents=True, exist_ok=True)
 
-    obofoundry_context = contexts_directory / 'obo_context.jsonld'
-    with obofoundry_context.open('w') as file:
-        json.dump(fp=file, indent=4, sort_keys=True, obj=_generate_context_json_ld(get_obofoundry_prefix_map()))
-
-
-def _generate_context_json_ld(prefix_map: Mapping[str, str]) -> Mapping[str, Mapping[str, str]]:
-    """Convert a prefix map into a JSON-LD context JSON"""
-    return {"@context": prefix_map}
+    with contexts_directory.joinpath('obo_context.jsonld').open('w') as file:
+        json.dump(fp=file, indent=4, sort_keys=True, obj={
+            "@context": get_obofoundry_prefix_map(),
+        })
 
 
 def get_obofoundry_prefix_map() -> Mapping[str, str]:
+    """Get the OBO foundry prefix map."""
     rv = {}
     for prefix in bioregistry.read_registry():
         obofoundry_prefix = bioregistry.get_obofoundry_prefix(prefix)
-        if obofoundry_prefix is None:
+        obofoundry_fmt = bioregistry.get_obofoundry_format(prefix)
+        if obofoundry_prefix is None or obofoundry_fmt is None:
             continue
-        rv[obofoundry_prefix] = f'http://purl.obolibrary.org/obo/{obofoundry_prefix.upper()}_'
-    return rv
-
-
-def _best():
-    rv = {}
-    for prefix in bioregistry.read_registry():
-        fmt = bioregistry.get_format(prefix)
-        if not fmt:
-            continue
-        if fmt.count('$1') > 1:
-            print('problem', prefix, fmt)
-            continue
-        if not fmt.endswith('$1'):
-            print('problem', prefix, fmt)
-            continue
-        rv[prefix] = fmt
+        rv[obofoundry_prefix] = obofoundry_fmt
     return rv
 
 
