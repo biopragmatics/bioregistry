18c4dac0062a367f225750a72b03da512ad4d33e
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-06-04 16:19:18 -0400

    Add new comparison for JSON graphs

--- src/bioregistry/__init__.py
+++ src/bioregistry/__init__.py
@@ -4,11 +4,11 @@
 
 from .resolve import (  # noqa
     get, get_banana, get_bioportal_prefix, get_collection, get_description, get_email, get_example,
-    get_fairsharing_prefix, get_format, get_homepage, get_identifiers_org_prefix, get_mappings, get_n2t_prefix,
-    get_name, get_obo_download, get_obofoundry_format, get_obofoundry_prefix, get_ols_prefix, get_owl_download,
-    get_pattern, get_pattern_re, get_provides_for, get_registry, get_registry_description, get_registry_example,
-    get_registry_homepage, get_registry_name, get_registry_url, get_synonyms, get_version, get_versions, has_terms,
-    is_deprecated, is_provider, namespace_in_lui, normalize_prefix, parse_curie,
+    get_fairsharing_prefix, get_format, get_homepage, get_identifiers_org_prefix, get_json_download, get_mappings,
+    get_n2t_prefix, get_name, get_obo_download, get_obofoundry_format, get_obofoundry_prefix, get_ols_prefix,
+    get_owl_download, get_pattern, get_pattern_re, get_provides_for, get_registry, get_registry_description,
+    get_registry_example, get_registry_homepage, get_registry_name, get_registry_url, get_synonyms, get_version,
+    get_versions, has_terms, is_deprecated, is_provider, namespace_in_lui, normalize_prefix, parse_curie,
 )
 from .resolve_identifier import (  # noqa
     get_bioportal_url, get_default_url, get_identifiers_org_curie, get_identifiers_org_url, get_link, get_n2t_url,
