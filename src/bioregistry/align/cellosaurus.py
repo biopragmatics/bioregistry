08a14093c7b69bf7474641c308a36444c943fb27
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-07-20 23:10:17 +0200

    Add skips

--- src/bioregistry/align/cellosaurus.py
+++ src/bioregistry/align/cellosaurus.py
@@ -2,7 +2,7 @@
 
 """Align Cellosaurus with the Bioregistry."""
 
-from typing import Sequence
+from typing import Mapping, Sequence
 
 from bioregistry.align.utils import Aligner
 from bioregistry.external import get_cellosaurus
@@ -18,6 +18,15 @@ class CellosaurusAligner(Aligner):
     key = "cellosaurus"
     getter = get_cellosaurus
     curation_header = ("name", "homepage", "category", "url")
+
+    def get_skip(self) -> Mapping[str, str]:
+        """Get the skipped Cellosaurus identifiers."""
+        return {
+            'Biosample': '',
+            'CCRID': 'not in english',
+            'CCTCC': 'dead site',
+            'CCLV': 'stub website, URL dead'
+        }
 
     def get_curation_row(self, external_id, external_entry) -> Sequence[str]:
         """Prepare curation rows for unaligned BioPortal registry entries."""
