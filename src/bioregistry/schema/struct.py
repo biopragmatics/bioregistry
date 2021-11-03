ba3ac3ecaf9161fc256ff05e67a1de9c6197bd37
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-11-03 01:39:33 +0100

    Use fullmatch
    This was in one of the suggestions made by daniel. thanks for making me aware!
    
    Co-Authored-By: Daniel Himmelstein <daniel.himmelstein@gmail.com>

--- src/bioregistry/schema/struct.py
+++ src/bioregistry/schema/struct.py
@@ -949,10 +949,7 @@ class Resource(BaseModel):
         pattern = self.get_pattern_re()
         if pattern is None:
             return None
-        match = pattern.match(identifier)
-        if not match:
-            return False
-        return match.group(0) == identifier
+        return pattern.fullmatch(identifier) is not None
 
     def is_known_identifier(self, identifier: str) -> Optional[bool]:
         """Check that a local unique identifier can be normalized and also matches a prefix's pattern."""
