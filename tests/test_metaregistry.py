ec052df7a753c1d8ce2f176c0456b91564652827
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-01-13 11:08:11 +0100

    Curate more contacts

--- tests/test_metaregistry.py
+++ tests/test_metaregistry.py
@@ -22,6 +22,7 @@ class TestMetaregistry(unittest.TestCase):
                 self.assertIn("homepage", data)
                 self.assertIn("example", data)
                 self.assertIn("description", data)
+                self.assertIsNotNone(registry_pydantic.contact)
 
                 if registry_pydantic.provider_uri_format:
                     self.assertIn("provider_uri_format", data)
