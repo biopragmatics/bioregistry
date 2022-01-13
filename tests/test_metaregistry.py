ca77cfb071f44f97aec7e1a25a193fa07b731d89
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-01-13 12:14:17 +0100

    Begin adding orcids

--- tests/test_metaregistry.py
+++ tests/test_metaregistry.py
@@ -23,6 +23,7 @@ class TestMetaregistry(unittest.TestCase):
                 self.assertIn("example", data)
                 self.assertIn("description", data)
                 self.assertIsNotNone(registry_pydantic.contact)
+                self.assertIsNotNone(registry_pydantic.contact.orcid, msg=registry_pydantic.contact.name)
                 self.assertNotEqual("FIXME", registry_pydantic.contact.name)
 
                 if registry_pydantic.provider_uri_format:
