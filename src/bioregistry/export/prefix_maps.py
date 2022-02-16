db1257737ea0b84d6818eaf90e85e9976789c201
kkaris <karis.klas@gmail.com>
2022-02-15 21:54:32 -0500

    Add missing .

--- src/bioregistry/export/prefix_maps.py
+++ src/bioregistry/export/prefix_maps.py
@@ -51,7 +51,7 @@ def _write_shacl(path: Path, prefix_map: Mapping[str, str]) -> None:
 [
     sh:declare
 {entries}
-]"""
+] ."""
     # Todo:
     #  1. Use URL from prefix_map or point to the OBO Foundry URL?
     #  2. Capitalize prefixes?
