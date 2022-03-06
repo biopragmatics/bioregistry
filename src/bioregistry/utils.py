27710d471b60b8e82a76c80e03ed9837f66a447b
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-03-06 12:13:18 +0100

    Include extra contributors in web app

--- src/bioregistry/utils.py
+++ src/bioregistry/utils.py
@@ -145,6 +145,9 @@ def read_contributors() -> Mapping[str, Attributable]:
     for resource in read_registry().values():
         if resource.contributor and resource.contributor.orcid:
             rv[resource.contributor.orcid] = resource.contributor
+        for contributor in resource.contributor_extras or []:
+            if contributor.orcid:
+                rv[contributor.orcid] = contributor
         if resource.reviewer and resource.reviewer.orcid:
             rv[resource.reviewer.orcid] = resource.reviewer
         contact = resource.get_contact()
@@ -166,6 +169,9 @@ def read_prefix_contributions() -> Mapping[str, Set[str]]:
     for prefix, resource in read_registry().items():
         if resource.contributor and resource.contributor.orcid:
             rv[resource.contributor.orcid].add(prefix)
+        for contributor in resource.contributor_extras or []:
+            if contributor.orcid:
+                rv[contributor.orcid].add(prefix)
     return dict(rv)
 
 
