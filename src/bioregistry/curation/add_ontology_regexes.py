3c4a35e0e0d93abb9fa1b5dab69781eee0ca4e8f
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-05-04 11:48:39 +0200

    Begin adding ontology patterns
    References #334

---
+++ src/bioregistry/curation/add_ontology_regexes.py
@@ -0,0 +1,25 @@
+import bioregistry
+
+
+def main():
+    registry = bioregistry.read_registry()
+    for prefix, resource in registry.items():
+        pattern = resource.get_pattern()
+        if pattern:
+            continue
+        example = resource.get_example()
+        if not example:
+            continue
+        obo_prefix = resource.get_obo_preferred_prefix()
+        if not obo_prefix:
+            continue
+        if not example.startswith("00"):
+            continue
+        pattern = f"^\\d{{{len(example)}}}$"
+        print(prefix, obo_prefix, example, pattern)
+        resource.pattern = pattern
+    bioregistry.write_registry(registry)
+
+
+if __name__ == '__main__':
+    main()
