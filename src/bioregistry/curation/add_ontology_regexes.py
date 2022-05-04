9ed4569acec78e9cbbd9f2bfda6e0bd326db7a01
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-05-04 11:55:49 +0200

    Add additional patterns

--- src/bioregistry/curation/add_ontology_regexes.py
+++ src/bioregistry/curation/add_ontology_regexes.py
@@ -1,8 +1,11 @@
-import bioregistry
+import tabulate
 
+import bioregistry
+import re
 
 def main():
     registry = bioregistry.read_registry()
+    rows = []
     for prefix, resource in registry.items():
         pattern = resource.get_pattern()
         if pattern:
@@ -10,16 +13,24 @@ def main():
         example = resource.get_example()
         if not example:
             continue
-        #obo_prefix = resource.get_obo_preferred_prefix()
-        #if not obo_prefix:
-        #    continue
-        if not example.startswith("00"):
-            continue
         if len(example) > 10:
-            continue
-        pattern = f"^\\d{{{len(example)}}}$"
-        print(prefix, example, pattern)
-        resource.pattern = pattern
+            continue  # skip long examples
+        if example.startswith("0"):
+            pattern = f"^\\d{{{len(example)}}}$"
+            if not re.match(pattern, example):
+                print("Check", prefix)
+                continue
+            resource.pattern = pattern
+            rows.append((prefix, example, resource.pattern))
+        elif example.isnumeric():
+            pattern = "^\\d+$"
+            if not re.match(pattern, example):
+                print("Check", prefix)
+                continue
+            resource.pattern = pattern
+            rows.append((prefix, example, resource.pattern))
+
+    print(tabulate.tabulate(rows, headers=["prefix", "example","pattern"]))
     bioregistry.write_registry(registry)
 
 
