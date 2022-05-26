3591a989a6d356fd110d66d3fef9462d34d72856
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-05-26 16:12:18 +0200

    Update curation burden calculation

--- src/bioregistry/summary.py
+++ src/bioregistry/summary.py
@@ -1,9 +1,14 @@
 """A script to generate summaries and text for the Bioregistry manuscript."""
 
 import datetime
+from collections import defaultdict
 from dataclasses import dataclass
+from itertools import combinations
 from textwrap import dedent
 from typing import Mapping
+
+from humanize import intword
+from rich import print
 
 import bioregistry
 from bioregistry.external import GETTERS
@@ -105,10 +110,83 @@ class BioregistrySummary:
         )
 
 
-def main():
-    summary = BioregistrySummary.make()
-    print(summary.paper_text())
+@dataclass
+class MappingBurdenSummary:
+    """A container for high-level statistics on the remaining mappings to be curated in the Bioregistry."""
+
+    #: This value is a bit of a strawman - it counts the total number of possible matches that could exist
+    #: between elements in all possible pairs of external registries. In reality, mappings should be
+    #: one-to-one (for the most part), meaning that once an element in one registry is mapped, it won't be
+    #: mapped to another one. This quadratically reduces the number in our estimate.
+    total_pairwise_upper_bound: int
+    pairwise_upper_bound: int
+    direct_upper_bound: int
+    remaining: int
+
+    @property
+    def pairwise_to_direct_ratio(self) -> float:
+        """Get the ratio between pairwise curation and direct curation."""
+        return self.pairwise_upper_bound / self.direct_upper_bound
+
+    def print(self):
+        """Print the summary."""
+        print(
+            dedent(
+                f"""\
+            While there is an upper bound of {intword(self.total_pairwise_upper_bound)} possible pairs of
+            records between pairs of external registries, there is a more realistic upper bound of
+            {self.pairwise_upper_bound:,} when assuming mappings between each pair of registries
+            are one-to-one. When using the Bioregistry as a hub for mappings, this upper bound
+            decreases by {self.pairwise_to_direct_ratio:.1f} times to {self.direct_upper_bound:,}.
+            Of these, a total of {self.remaining:,} ({self.remaining/self.direct_upper_bound:.0%})
+            mappings remain to be curated.
+            """
+            )
+            .strip()
+            .replace("\n", " ")
+        )
+
+    @classmethod
+    def make(cls):
+        """Instantiate the class."""
+        registry_to_prefixes = {metaprefix: set(getter()) for metaprefix, _, getter in GETTERS}
+
+        total_pairwise_upper_bound = sum(
+            len(x) * len(y) for x, y in combinations(registry_to_prefixes.values(), 2)
+        )
+        exclusive_pairwise_upper_bound = sum(
+            min(len(x), len(y)) for x, y in combinations(registry_to_prefixes.values(), 2)
+        )
+        exclusive_direct_upper_bound = sum(len(x) for x in registry_to_prefixes.values())
+        ratio = exclusive_pairwise_upper_bound / exclusive_direct_upper_bound
+
+        registry = bioregistry.read_registry()
+        registry_to_mapped_prefixes = defaultdict(set)
+        for prefix, resource in registry.items():
+            for metaprefix, external_prefix in resource.get_mappings().items():
+                registry_to_mapped_prefixes[metaprefix].add(external_prefix)
+
+        # Get the set of prefixes for each external registry that haven't been
+        # mapped to the Bioregistry
+        registry_to_unmapped_prefixes = {
+            metaprefix: external_prefixes - registry_to_mapped_prefixes[metaprefix]
+            for metaprefix, external_prefixes in registry_to_prefixes.items()
+        }
+        remaining = sum(len(v) for v in registry_to_unmapped_prefixes.values())
+
+        return cls(
+            total_pairwise_upper_bound=total_pairwise_upper_bound,
+            pairwise_upper_bound=exclusive_pairwise_upper_bound,
+            direct_upper_bound=exclusive_direct_upper_bound,
+            remaining=remaining,
+        )
+
+
+def _main():
+    MappingBurdenSummary.make().print()
+    print()
+    print(BioregistrySummary.make().paper_text())
 
 
 if __name__ == "__main__":
-    main()
+    _main()
