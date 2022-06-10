f6edca0917c1f04928784a970dbe4297bf32e19e
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-06-10 14:45:43 +0200

    Add better examples

--- src/bioregistry/pandas.py
+++ src/bioregistry/pandas.py
@@ -112,10 +112,30 @@ def validate_prefixes(
     :param column: The column of prefixes to validate
     :param target_column:
         The optional column to put the results of validation
+
+    .. code-block:: python
+
+        import bioregistry.pandas as brpd
+        import pandas as pd
+
+        df = pd.read_csv(
+            "http://geneontology.org/gene-associations/goa_human.gaf.gz",
+            sep="\t",
+            comment='!',
+            header=None,
+        )
+
+        # column 1: DB
+        #  i.e., `UniProtKB` entries are not standard, and are therefore false
+        idx = brpd.validate_prefixes(df, column=0)
+
+        # Slice the dataframe based on valid and invalid prefixes
+        valid_prefix_df = df[idx]
+        invalid_prefix_df = df[~idx]
     """
     column = _norm_column(df, column)
     results = df[column].map(
-        lambda x: bioregistry.normalize_prefix(x) is not None, na_action="ignore"
+        lambda x: x == bioregistry.normalize_prefix(x), na_action="ignore"
     )
     if target_column:
         df[target_column] = results
@@ -130,22 +150,7 @@ def validate_curies(
     :param df: A DataFrame
     :param column: The column of CURIEs to validate
     :param target_column:
-        The optional column to put the results of validation
-    """
-    column = _norm_column(df, column)
-    results = df[column].map(bioregistry.is_valid_curie, na_action="ignore")
-    if target_column:
-        df[target_column] = results
-    return results
-
-
-def validate_identifiers(
-    df: pd.DataFrame,
-    column: Union[int, str],
-    prefix: str,
-    target_column: Optional[str] = None,
-) -> pd.Series:
-    """Validate local unique identifiers in a given column.
+        The optional column to put the results of validation.
 
     .. code-block:: python
 
@@ -159,17 +164,71 @@ def validate_identifiers(
             header=None,
         )
 
-        # column 17: note that invalid CURIEs are written like UniProtKB:P12345-2,
-        #  where these refer to isoforms and should use the prefix `uniprot.isoform`
-        results = brpd.validate_identifiers(df, column="Gene Product Form ID")
+        # column 5: GO ID - fix normalization of capitalization of prefix,
+        #  i.e., `GO:0003993` is not standard and is therefore false
+        idx = brpd.validate_curies(df, column=4)
+
+        # Slice the dataframe
+        valid_go_df = df[idx]
+        invalid_go_df = df[~idx]
     """
     column = _norm_column(df, column)
-    norm_prefix = bioregistry.normalize_prefix(prefix)
-    if norm_prefix is None:
+    results = df[column].map(bioregistry.is_valid_curie, na_action="ignore")
+    if target_column:
+        df[target_column] = results
+    return results
+
+
+def validate_identifiers(
+    df: pd.DataFrame,
+    column: Union[int, str],
+    prefix: Optional[str] = None,
+    prefix_column: Optional[str] = None,
+    target_column: Optional[str] = None,
+) -> pd.Series:
+    """Validate local unique identifiers in a given column.
+
+    Some data sources split the prefix and identifier in separate columns,
+    so you can use the ``prefix_column`` argument instead of the ``prefix``
+    argument like in the following example with the GO Annotation Database:
+
+    .. code-block:: python
+
+        import bioregistry.pandas as brpd
+        import pandas as pd
+
+        df = pd.read_csv(
+            "http://geneontology.org/gene-associations/goa_human.gaf.gz",
+            sep="\t",
+            comment='!',
+            header=None,
+        )
+
+        # Use a combination of column 1 (DB) and column 2 (DB Object ID) for validation
+        idx = brpd.validate_identifiers(df, column=1, prefix_column=0)
+
+        # Split the dataframe based on valid and invalid identifiers
+        valid_df = df[idx]
+        invalid_df = df[~idx]
+    """
+    column = _norm_column(df, column)
+    if prefix_column is None and prefix is None:
         raise ValueError
-    results = df[column].map(
-        functools.partial(bioregistry.is_valid_identifier, prefix=norm_prefix), na_action="ignore"
-    )
+    elif prefix_column is not None and prefix is not None:
+        raise ValueError
+    elif prefix is not None:
+        norm_prefix = bioregistry.normalize_prefix(prefix)
+        if norm_prefix is None:
+            raise ValueError
+        results = df[column].map(
+            functools.partial(bioregistry.is_valid_identifier, prefix=norm_prefix),
+            na_action="ignore",
+        )
+    else:  # prefix_column is not None
+        results = df.applymap(
+            lambda row: bioregistry.is_valid_identifier(row[prefix_column], row[column]),
+            na_action="ignore",
+        )
     if target_column:
         df[target_column] = results
     return results
@@ -182,7 +241,29 @@ def identifiers_to_curies(
     prefix_column: Optional[str] = None,
     target_column: Optional[str] = None,
 ) -> None:
-    """Convert a column of local unique identifiers to CURIEs."""
+    """Convert a column of local unique identifiers to CURIEs.
+
+    .. code-block:: python
+
+        import bioregistry.pandas as brpd
+        import pandas as pd
+
+        df = pd.read_csv(
+            "http://geneontology.org/gene-associations/goa_human.gaf.gz",
+            sep="\t",
+            comment='!',
+            header=None,
+        )
+
+        # column 17: Gene Product Form ID - note that invalid CURIEs are written like UniProtKB:P12345-2,
+        #  where these refer to isoforms and should use the prefix `uniprot.isoform`
+        idx = brpd.validate_identifiers(df, column=16)
+
+        # Get a dataframe of the valid and invalid rows
+        valid_gene_products_df = df[idx]
+        invalid_gene_products_df = df[~idx]
+
+    """
     column = _norm_column(df, column)
     if prefix_column is None and prefix is None:
         raise ValueError
@@ -193,13 +274,15 @@ def identifiers_to_curies(
         if norm_prefix is None:
             raise ValueError
         df[target_column or column] = df[column].map(
-            lambda s: f"{norm_prefix}:{s}", na_action="ignore"
+            functools.partial(bioregistry.curie_to_str, prefix=norm_prefix),
+            na_action="ignore",
         )
     else:  # prefix_column is not None
         prefix_column = _norm_column(df, prefix_column)
-        df[[column, prefix_column]]
-
-        raise NotImplementedError
+        df[target_column or column] = df.applymap(
+            lambda row: bioregistry.curie_to_str(row[prefix_column], row[column]),
+            na_action="ignore",
+        )
 
 
 def identifiers_to_iris(
