56e267e4c536890da960f8dffc5e634af6f7f0cd
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-08-12 00:33:48 +0200

    Update pages

--- src/bioregistry/benchmarks/curie_parsing.py
+++ src/bioregistry/benchmarks/curie_parsing.py
@@ -1,6 +1,9 @@
 """A benchmark for Bioregistry's CURIE parser."""
 
+import itertools as itt
+import random
 import time
+from statistics import mean
 from typing import Iterable, Tuple
 
 import matplotlib.pyplot as plt
@@ -10,13 +13,14 @@ from tqdm import tqdm
 import bioregistry
 from bioregistry import manager
 from bioregistry.constants import CURIE_PARSING_DATA_PATH, CURIE_PARSING_SVG_PATH
-import itertools as itt
 
 
 def get_curies(rebuild: bool = True):
     """Get prefix-identifier-banana-curie tuples for benchmarking."""
     if CURIE_PARSING_DATA_PATH.is_file() and not rebuild:
-        return [line.strip().split("\t") for line in CURIE_PARSING_DATA_PATH.read_text().splitlines()]
+        return [
+            line.strip().split("\t") for line in CURIE_PARSING_DATA_PATH.read_text().splitlines()
+        ]
     rows = sorted(set(iter_curies()))
     CURIE_PARSING_DATA_PATH.write_text("\n".join("\t".join(line) for line in rows))
     return rows
@@ -50,7 +54,7 @@ def iter_curies() -> Iterable[Tuple[str, str, str, str]]:
                 yield prefix, e, p, banana_extended, f"{p}:{example_extended}"
 
 
-def main(rebuild: bool = False):
+def main(rebuild: bool = False, epochs: int = 3):
     """Test parsing CURIEs."""
     curies = get_curies(rebuild=rebuild)
 
@@ -58,17 +62,27 @@ def main(rebuild: bool = False):
     bioregistry.parse_curie("DRON:00023232")
 
     times = []
-    for prefix, synonym, identifier, banana, curie in tqdm(curies, unit_scale=True, unit="URI"):
-        start = time.time()
-        p, i = bioregistry.parse_curie(curie)
-        times.append(time.time() - start)
-        if p != prefix or i != identifier:
-            tqdm.write(f"failed on {curie} to get {prefix}:{identifier}")
+    failures = 0
+    for _ in range(epochs):
+        random.shuffle(curies)
+        for prefix, identifier, _synonym, _banana, curie in tqdm(
+            curies, unit_scale=True, unit="CURIE"
+        ):
+            start = time.time()
+            p, i = bioregistry.parse_curie(curie)
+            times.append(time.time() - start)
+            if p != prefix or i != identifier:
+                failures += 1
+                tqdm.write(f"failed on {curie} to get {prefix}:{identifier}")
 
     fig, ax = plt.subplots()
     sns.histplot(data=times, ax=ax, log_scale=True)
+    m = mean(times)
+    ax.axvline(m)
     ax.set_xlabel("Time (seconds)")
-    ax.set_title("Bioregistry CURIE Parsing Benchmark")
+    ax.set_title(
+        f"Bioregistry CURIE Parsing Benchmark\nAverage: {round(1/m):,} CURIE/s, Errors: {failures // epochs}"
+    )
     fig.savefig(CURIE_PARSING_SVG_PATH)
 
 
