0a5337c58e120623151ab5ac7d6524818273a44d
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-08-12 00:39:21 +0200

    Pass mypy

--- src/bioregistry/benchmarks/curie_parsing.py
+++ src/bioregistry/benchmarks/curie_parsing.py
@@ -26,7 +26,7 @@ def get_curies(rebuild: bool = True):
     return rows
 
 
-def iter_curies() -> Iterable[Tuple[str, str, str, str]]:
+def iter_curies() -> Iterable[Tuple[str, str, str, str, str]]:
     """Generate prefix-identifier-banana-curie tuples for benchmarking."""
     for prefix, resource in tqdm(
         manager.registry.items(), desc="Generating test CURIEs", unit="prefix"
