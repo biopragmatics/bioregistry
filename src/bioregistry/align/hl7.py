4624ce2afcbc0e2abeca3b97afdac0d972e12377
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-09-26 14:21:17 +0200

    Update docs

--- src/bioregistry/align/hl7.py
+++ src/bioregistry/align/hl7.py
@@ -15,20 +15,21 @@ __all__ = [
 class HL7Aligner(Aligner):
     """Aligner for HL7 External Code Systems."""
 
+    # corresponds to the metaprefix in metaregistry.json
     key = "hl7"
-    alt_key_match = "preferred_prefix"
-    getter = get_hl7
-    curation_header = ("status", "preferred_prefix", "name", "homepage", "description")
 
-    def get_curation_row(self, external_id, external_entry) -> Sequence[str]:
-        """Prepare curation rows for unaligned OID entries."""
-        return [
-            external_entry["status"],
-            external_entry["preferred_prefix"],
-            external_entry.get("name") or "",
-            external_entry.get("homepage") or "",
-            external_entry.get("description") or "",
-        ]
+    # This key tells the aligner that the prefix might not be super informative for
+    # lexical matching (in this case, they're OIDs, so definitely not helpful)
+    # and that there's another key inside each record that might be better
+    alt_key_match = "preferred_prefix"
+
+    # This function gets the dictionary of prefix -> record. Note that it's not
+    # called but only passed by reference.
+    getter = get_hl7
+
+    # This lists all of the keys inside each record to be displayed in the curation
+    # sheet. Below, the
+    curation_header = ("status", "preferred_prefix", "name", "homepage", "description")
 
 
 if __name__ == "__main__":
