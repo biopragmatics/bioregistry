6afa4dbcd71e859934f310b013bc9722db82a014
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-10-02 21:44:24 +0200

    Update constants.py

--- src/bioregistry/app/constants.py
+++ src/bioregistry/app/constants.py
@@ -39,9 +39,7 @@ BIOSCHEMAS = {
         "name": "Harvard Medical School",
         "url": "https://hms.harvard.edu/",
     },
-    "citation": [
-        "https://bioregistry.io/doi:10.1101/2022.07.08.499378",
-    ],
+    "citation": "https://bioregistry.io/doi:10.1101/2022.07.08.499378",
     "distribution": [
         {
             "contentUrl": "https://github.com/biopragmatics/bioregistry/blob/main/docs/_data/bioregistry.ttl",
