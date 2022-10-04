3be0e0ea329c9951ae6b4c0c562c35e98ce7ccf6
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-10-05 01:28:47 +0200

    Update bibliometrics.py

--- src/bioregistry/bibliometrics.py
+++ src/bioregistry/bibliometrics.py
@@ -1,16 +1,21 @@
+"""Utilities for publications."""
+
 import typing
 from collections import Counter
 from typing import Iterable, List
 
+import click
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
 from bioregistry import manager
+from bioregistry.constants import DOCS_IMG
 from bioregistry.schema.struct import Publication, deduplicate_publications
 
 
 def get_oldest_publications() -> List[Publication]:
+    """Get the oldest publication (by year) for each resource."""
     return deduplicate_publications(
         min(publications, key=lambda p: p.year or 0)
         for resource in manager.registry.values()
@@ -19,6 +24,7 @@ def get_oldest_publications() -> List[Publication]:
 
 
 def get_all_publications() -> List[Publication]:
+    """Get all publications for each resource."""
     return deduplicate_publications(
         publication
         for resource in manager.registry.values()
@@ -26,11 +32,14 @@ def get_all_publications() -> List[Publication]:
     )
 
 
-def count_publication_years(publications: Iterable[Publication]) -> typing.Counter[int]:
+def count_publication_years(
+    publications: Iterable[Publication], minimum_year: int = 1995
+) -> typing.Counter[int]:
+    """Count the number of publications for resources in the Bioregistry for each year."""
     year_counter = Counter(
-        p.year
-        for p in publications
-        if p.year and p.year > 1995
+        publication.year
+        for publication in publications
+        if publication.year and publication.year >= minimum_year
     )
     for i in range(min(year_counter) - 1, max(year_counter) + 1):
         if i not in year_counter:
@@ -38,7 +47,9 @@ def count_publication_years(publications: Iterable[Publication]) -> typing.Count
     return year_counter
 
 
+@click.command()
 def main():
+    """Generate images."""
     publications = get_oldest_publications()
     year_counter = count_publication_years(publications)
     df = pd.DataFrame(sorted(year_counter.items()), columns=["year", "count"])
@@ -50,7 +61,7 @@ def main():
     ax.set_title(f"Timeline of {len(publications):,} Publications")
     plt.xticks(rotation=45)
     fig.tight_layout()
-    fig.savefig("/Users/cthoyt/Desktop/test.png", dpi=350)
+    fig.savefig(DOCS_IMG.joinpath("bibliography_years.png"), dpi=350)
 
 
 if __name__ == "__main__":
