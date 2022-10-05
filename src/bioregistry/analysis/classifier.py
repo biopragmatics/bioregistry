0142220c680887c719edd66aa04e5a94c5796bd4
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-10-05 19:29:30 +0200

    Add output

--- src/bioregistry/analysis/classifier.py
+++ src/bioregistry/analysis/classifier.py
@@ -8,36 +8,42 @@ from sklearn.metrics import matthews_corrcoef, roc_auc_score
 from sklearn.metrics import confusion_matrix
 
 URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRPtP-tcXSx8zvhCuX6fqz_QvHowyAoDahnkixARk9rFTe0gfBN9GfdG6qTNQHHVL0i33XGSp_nV9XM/pub?gid=0&single=true&output=tsv"
+MODULE = pystow.module("bioregistry", "analysis")
 
 
 def _map(s: str):
     if s in {"1", "1.0", 1, 1.0}:
-        return True
+        return 1
     if s in {"0", "0.0", 0, 0.0}:
-        return False
-    raise ValueError(s)
+        return 0
+    return None
 
 
 def main():
-    print("downloading curation")
-    curation_df = pystow.ensure_csv("bioregistry", "analysis", url=URL, name="ben_curation.tsv")
-    curation_df = curation_df[["pubmed", "title", "relevant"]]
-    curation_df = curation_df[curation_df["relevant"].notna()]
-    curation_df["label"] = curation_df["relevant"].map(_map)
-    del curation_df["relevant"]
-
     print("loading bioregistry publications")
     publication_df = get_publications_df()
     publication_df = publication_df[publication_df.pubmed.notna()]
     publication_df = publication_df[["pubmed", "title"]]
     publication_df["label"] = True
 
+    print("downloading curation")
+    curation_df = MODULE.ensure_csv(url=URL, name="ben_curation.tsv")
+    curation_df["label"] = curation_df["relevant"].map(_map)
+    curation_df = curation_df[["pubmed", "title", "label"]]
+
     df = pd.concat([curation_df, publication_df])
 
-    print("vectorizing")
+    print("labels:", df.label.unique())
+
+    print("training tf-idf")
     vectorizer = TfidfVectorizer()
-    x = vectorizer.fit_transform(df.title)
-    y = df.label
+    vectorizer.fit(df.title)
+
+    print("applying tf-idf")
+    annotation_idx = df.label.notna()
+    annotated_df = df[annotation_idx]
+    x = vectorizer.transform(annotated_df.title)
+    y = annotated_df.label
 
     print("splitting")
     x_train, x_test, y_train, y_test = train_test_split(
@@ -53,8 +59,15 @@ def main():
     mcc = matthews_corrcoef(y_test, y_pred)
     roc_auc = roc_auc_score(y_test, clf.predict_log_proba(x_test)[:, 1])
     print(f"mcc: {mcc:.2f}, auc-roc: {roc_auc:.2f}")
-
     print(confusion_matrix(y_test, y_pred))
+
+    print("predicting on unknowns")
+    novel_df = df[~annotation_idx][["pubmed", "title"]].copy()
+    novel_df["score"] = clf.predict_proba(vectorizer.transform(novel_df.title))[:, 1]
+    novel_df = novel_df.sort_values("score", ascending=False)
+    path = MODULE.join(name="results.tsv")
+    print(f"writing to {path}")
+    novel_df.to_csv(path, sep='\t', index=False)
 
 
 if __name__ == '__main__':
