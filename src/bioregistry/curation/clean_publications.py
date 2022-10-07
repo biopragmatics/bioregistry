72a11fca9ae407254dbbd110c6da559eb76cb5c1
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-10-07 13:09:23 +0200

    Update publication enrichment

--- src/bioregistry/curation/clean_publications.py
+++ src/bioregistry/curation/clean_publications.py
@@ -6,6 +6,7 @@ Run this script with python -m bioregistry.curation.clean_publications.
 from functools import lru_cache
 from typing import Optional
 
+from manubot.cite.doi import get_doi_csl_item
 from manubot.cite.pubmed import get_pmid_for_doi, get_pubmed_csl_item
 from tqdm import tqdm
 
@@ -16,7 +17,15 @@ from bioregistry.utils import removeprefix
 
 @lru_cache(None)
 def _get_pubmed_csl_item(pmid):
-    return get_pubmed_csl_item(pmid)
+    try:
+        return get_pubmed_csl_item(pmid)
+    except Exception:
+        return None
+
+
+@lru_cache(None)
+def _get_doi_csl_item(pmid):
+    return get_doi_csl_item(pmid)
 
 
 @lru_cache(None)
@@ -28,7 +37,7 @@ def _get_pubmed_from_doi(doi: str) -> Optional[str]:
 def _main():
     c = 0
 
-    dois = set()
+    resource_dois = []
     resources = []
     it = tqdm(manager.registry.values(), unit="resource", unit_scale=True, desc="caching PMIDs")
     for resource in it:
@@ -36,31 +45,35 @@ def _main():
         it.set_postfix(prefix=resource.prefix)
         resource_publications = resource.get_publications()
         pubmed_ids = set()
+        dois = set()
         for publication in resource_publications:
             if publication.pubmed:
                 pubmed_ids.add(publication.pubmed)
             elif publication.doi:
                 doi = removeprefix(publication.doi, "https://doi.org/")
-                dois.add(doi)
                 tqdm.write(f"getting pubmed from DOI:{doi}")
-                pmid = _get_pubmed_from_doi(doi)
-                if pmid:
-                    pubmed_ids.add(pmid)
+                pubmed = _get_pubmed_from_doi(doi)
+                if pubmed:
+                    pubmed_ids.add(pubmed)
+                else:
+                    dois.add(doi)
         if pubmed_ids:
             resources.append((resource, pubmed_ids))
-        elif resource.publications:
+        if dois:
+            resource_dois.append((resource, dois))
+        if not pubmed_ids and not dois and resource.publications:
             resource.publications = deduplicate_publications(resource.publications)
 
-    tqdm.write(f"looked up {len(dois):,} DOIs")
+    tqdm.write(f"looked up {len(resource_dois):,} DOIs")
 
-    for resource, pubmed_ids in tqdm(
-        resources, desc="resources with pubmeds to update", unit="resource"
-    ):
+    for resource, dois in tqdm(resource_dois):
         new_publications = []
-        for pubmed in pubmed_ids:
-            csl_item = _get_pubmed_csl_item(pubmed)
+        for doi in dois:
+            csl_item = _get_doi_csl_item(doi)
+            if not csl_item:
+                continue
             title = csl_item.get("title", "").strip() or None
-            doi = csl_item.get("DOI") or None
+            pubmed = csl_item.get("PMID") or None
             pmc = csl_item.get("PMCID") or None
             year = csl_item.get("issued", {}).get("date-parts", [[None]])[0][0]
             if not title:
@@ -71,6 +84,47 @@ def _main():
                     pubmed=pubmed,
                     title=title,
                     doi=doi and doi.lower(),
+                    pmc=pmc,
+                    year=year,
+                )
+            )
+        _pubs = [
+            *(new_publications or []),
+            *(resource.publications or []),
+        ]
+        if len(_pubs) == 1:
+            resource.publications = _pubs
+        else:
+            resource.publications = deduplicate_publications(_pubs)
+
+        c += 1
+        if c > 7:
+            # output every so often in case of failure
+            manager.write_registry()
+            c = 0
+
+    for resource, pubmed_ids in tqdm(
+        resources, desc="resources with pubmeds to update", unit="resource"
+    ):
+        new_publications = []
+        for pubmed in pubmed_ids:
+            csl_item = _get_pubmed_csl_item(pubmed)
+            if not csl_item:
+                continue
+            title = csl_item.get("title", "").strip() or None
+            doi = csl_item.get("DOI") or None
+            if doi:
+                doi = removeprefix(doi.lower(), "https://doi.org/")
+            pmc = csl_item.get("PMCID") or None
+            year = csl_item.get("issued", {}).get("date-parts", [[None]])[0][0]
+            if not title:
+                tqdm.write(f"No title available for pubmed:{pubmed} / doi:{doi} / pmc:{pmc}")
+                continue
+            new_publications.append(
+                Publication(
+                    pubmed=pubmed,
+                    title=title,
+                    doi=doi,
                     pmc=pmc,
                     year=year,
                 )
