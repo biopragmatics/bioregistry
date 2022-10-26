76756380cc4dfc054f0ba69de22b72102322cf15
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-10-26 23:34:13 +0200

    Update import

--- src/bioregistry/external/bartoc.py
+++ src/bioregistry/external/bartoc.py
@@ -9,29 +9,32 @@ PROCESSED_PATH = DIRECTORY / "processed.json"
 URL = "https://bartoc.org/data/dumps/latest.ndjson"
 
 
-def get_bartoc(force: bool = False)
+def get_bartoc(force: bool = True):
     if PROCESSED_PATH.is_file() and not force:
         return json.loads(PROCESSED_PATH.read_text())
     rv = {}
     for line in requests.get(URL).iter_lines():
         record = json.loads(line)
         record = process_record(record)
-        rv[record["prefix"]] = rv
+        rv[record["prefix"]] = record
 
-    PROCESSED_PATH.write_text(json.dumps(rv, indent=2, ensure_ascii=True, sort_keys=True))
+    PROCESSED_PATH.write_text(json.dumps(rv, indent=2, ensure_ascii=False, sort_keys=True))
     return rv
 
 
 def process_record(record):
     rv = {
         "prefix": record["uri"][len("http://bartoc.org/en/node/"):],
-        "description": record.get("definition", {}).get("en")
+        "description": record.get("definition", {}).get("en", [""])[0].strip('"').strip(),
         "homepage": record.get("url"),
         "name": record.get("prefLabel", {}).get("en"),
-        "pattern": record.get("notationPattern"),
     }
-    for license in record.get("license", []):
-        license_key = standardize_license(license["uri"])
+    pattern = record.get("notationPattern")
+    if pattern:
+        rv["pattern"] = "^" + pattern.lstrip("^").rstrip("$") + "$"
+
+    for license_dict in record.get("license", []):
+        license_key = standardize_license(license_dict["uri"])
         if license_key:
             rv["license"] = license_key
 
