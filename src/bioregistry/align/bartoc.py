6109bcfc2774af87b52ca852ac4f37a477137787
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-10-27 15:23:41 +0200

    Align with BARTOC (#643)
    Closes #568

---
+++ src/bioregistry/align/bartoc.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+"""Align the BARTOC with the Bioregistry."""
+
+from bioregistry.align.utils import Aligner
+from bioregistry.external.bartoc import get_bartoc
+
+__all__ = [
+    "BartocAligner",
+]
+
+
+class BartocAligner(Aligner):
+    """Aligner for BARTOC."""
+
+    key = "bartoc"
+    getter = get_bartoc
+    alt_key_match = "abbreviation"
+    curation_header = ["name", "homepage", "description"]
+
+
+if __name__ == "__main__":
+    BartocAligner.align()
