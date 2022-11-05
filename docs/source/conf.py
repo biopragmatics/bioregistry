fd7b9c756781be379d613e0c43cdc12628993442
GitHub Action <action@github.com>
2022-11-05 01:30:14 +0000

    Bump version: 0.6.5-dev → 0.6.5

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ copyright = f"{date.today().year}, Charles Tapley Hoyt"
 author = "Charles Tapley Hoyt"
 
 # The full version, including alpha/beta/rc tags.
-release = "0.6.5-dev"
+release = "0.6.5"
 
 # The short X.Y version.
 parsed_version = re.match(
