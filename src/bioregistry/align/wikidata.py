c2a643c19a08e9f98f8058c3b1ba161b1e0d148c
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-11-16 11:59:34 +0100

    More curation

--- src/bioregistry/align/wikidata.py
+++ src/bioregistry/align/wikidata.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 """Align Wikidata with the Bioregistry."""
+from typing import Mapping
 
 from bioregistry.align.utils import Aligner
 from bioregistry.external.wikidata import get_wikidata
@@ -27,6 +28,29 @@ SKIP = {
     "P3337": "is a data property",
     "P3485": "is a data property",
     "P3486": "is a data property",
+    "P10322": "is a data property",
+    "P10630": "is a data property",
+    "P1193": "is a data property",
+    "P1603": "is a data property",
+    "P2067": "is a data property",
+    "P2844": "is a data property",
+    "P2854": "is a data property",
+    "P3487": "is a data property",
+    "P3492": "is a data property",
+    "P4214": "is a data property",
+    "P3488": "is a data property",
+    "P4250": "is a data property",
+    "P574": "is a data property",
+    "P7770": "is a data property",
+    "P783": "is a data property",
+    "P7862": "is a data property",
+    "P8010": "is a data property",
+    "P8011": "is a data property",
+    "P8049": "is a data property",
+    "P8556": "is a data property",
+    "P9107": "is a data property",
+    "Q112586709": "should not be annotated like a property",
+    "Q111831044": "should not be annotated like a property",
 }
 
 class WikidataAligner(Aligner):
@@ -36,6 +60,9 @@ class WikidataAligner(Aligner):
     getter = get_wikidata
     curation_header = ("name", "homepage", "description", "uri_format", "example")
 
+    def get_skip(self) -> Mapping[str, str]:
+        """Get entries to skip."""
+        return SKIP
 
 if __name__ == "__main__":
     WikidataAligner.align()
