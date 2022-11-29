01cd11290c3d39442316ae17771d8ba2aafd868e
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-11-29 20:23:21 +0100

    Add banana to `agrovoc` and update resolver (#661)
    Closes #660

--- src/bioregistry/app/ui.py
+++ src/bioregistry/app/ui.py
@@ -276,8 +276,8 @@ def resolve(prefix: str, identifier: Optional[str] = None):
     2. The prefix has a validation pattern and the identifier does not match it
     3. There are no providers available for the URL
     """  # noqa:DAR101,DAR201
-    norm_prefix = manager.normalize_prefix(prefix)
-    if norm_prefix is None:
+    _resource = manager.get_resource(prefix)
+    if _resource is None:
         return (
             render_template(
                 "resolve_errors/missing_prefix.html", prefix=prefix, identifier=identifier
@@ -285,10 +285,12 @@ def resolve(prefix: str, identifier: Optional[str] = None):
             404,
         )
     if identifier is None:
-        return redirect(url_for("." + resource.__name__, prefix=norm_prefix))
+        return redirect(url_for("." + resource.__name__, prefix=_resource.prefix))
 
-    pattern = manager.get_pattern(prefix)
-    if pattern and not manager.is_standardizable_identifier(prefix, identifier):
+    identifier = _resource.standardize_identifier(identifier)
+
+    pattern = _resource.get_pattern()
+    if pattern and not _resource.is_valid_identifier(identifier):
         return (
             render_template(
                 "resolve_errors/invalid_identifier.html",
