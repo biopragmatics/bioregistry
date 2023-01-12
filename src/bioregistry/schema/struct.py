200dadf84781d45f1ab3683c999fac38f8053d95
Charles Tapley Hoyt <cthoyt@gmail.com>
2023-01-12 12:18:28 +0100

    Add bioschemas annotations (#610)
    Closes #160

--- src/bioregistry/schema/struct.py
+++ src/bioregistry/schema/struct.py
@@ -1778,6 +1778,13 @@ class Resource(BaseModel):
                 return license_value
         return None
 
+    def get_license_url(self) -> Optional[str]:
+        """Get a license URL."""
+        spdx_id = self.get_license()
+        if spdx_id is None:
+            return None
+        return f"{BIOREGISTRY_REMOTE_URL}/spdx:{spdx_id}"
+
     def get_version(self) -> Optional[str]:
         """Get the version for the resource."""
         if self.version:
@@ -1803,6 +1810,34 @@ class Resource(BaseModel):
 
         rv = cast(str, removesuffix(removeprefix(markdown(rv), "<p>"), "</p>"))
         return markupsafe.Markup(rv)
+
+    def get_bioschemas_jsonld(self):
+        """Get the BioSchemas JSON-LD."""
+        identifiers = [
+            f"bioregistry:{self.prefix}",
+            *(
+                f"{metaprefix}:{metaidentifier}"
+                for metaprefix, metaidentifier in self.get_mappings().items()
+            ),
+        ]
+
+        rv = {
+            "@context": "https://schema.org",
+            "@type": "Dataset",
+            "@id": f"{BIOREGISTRY_REMOTE_URL}/{self.prefix}",
+            "url": self.get_homepage(),
+            "name": self.get_name(),
+            "description": self.get_description(),
+            "identifier": identifiers,
+            "keywords": self.get_keywords(),
+        }
+        version = self.get_version()
+        if version:
+            rv["version"] = version
+        license_url = self.get_license_url()
+        if license_url:
+            rv["license"] = license_url
+        return rv
 
 
 SchemaStatus = Literal["required", "required*", "present", "present*", "missing"]
