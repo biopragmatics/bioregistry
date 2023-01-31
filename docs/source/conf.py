a464af2a887eaa588b856dd21448b8f196b61a46
GitHub Action <action@github.com>
2023-01-31 01:24:39 +0000

    Bump version: 0.6.63-dev → 0.6.63

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ copyright = f"{date.today().year}, Charles Tapley Hoyt"
 author = "Charles Tapley Hoyt"
 
 # The full version, including alpha/beta/rc tags.
-release = "0.6.63-dev"
+release = "0.6.63"
 
 # The short X.Y version.
 parsed_version = re.match(
--- src/bioregistry/version.py
+++ src/bioregistry/version.py
@@ -12,7 +12,7 @@ __all__ = [
     "get_git_hash",
 ]
 
-VERSION = "0.6.63-dev"
+VERSION = "0.6.63"
 
 
 def get_git_hash() -> Optional[str]:
