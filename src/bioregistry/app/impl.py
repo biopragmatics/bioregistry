fbc29f281c72da84bf93f172202e5b5f04cdfedf
Charles Tapley Hoyt <cthoyt@gmail.com>
2023-03-24 09:07:15 +0100

    Update impl.py
    Co-Authored-By: Vincent Emonet <vincent.emonet@gmail.com>

--- src/bioregistry/app/impl.py
+++ src/bioregistry/app/impl.py
@@ -179,7 +179,7 @@ def get_app(
     app.jinja_env.globals.update(manager=app.manager, curie_to_str=curie_to_str)
 
     fast_api = FastAPI()
-    fast_api.mount("/sparql", _get_sparql_router(app.manager))
+    fast_api.include_router(_get_sparql_router(app.manager))
     fast_api.mount("/", WSGIMiddleware(app))
     if return_flask:
         return fast_api, app
@@ -190,7 +190,7 @@ def _get_sparql_router(manager: Manager) -> APIRouter:
     sparql_graph = MappingServiceGraph(converter=manager.converter)
     sparql_processor = MappingServiceSPARQLProcessor(graph=sparql_graph)
     sparql_router = SparqlRouter(
-        path="/",
+        path="/sparql",
         title="Bioregistry SPARQL Service",
         description="An identifier mapping service",
         version=version.get_version(),
