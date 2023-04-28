be6d9e5c582ef27c56f3b41b5c80e3cf2823c7ad
Charles Tapley Hoyt <cthoyt@gmail.com>
2023-04-28 16:04:34 +0200

    Address sphinx and flask churn (#812)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -227,7 +227,7 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
-    "https://docs.python.org/3/": None,
+    "python": ("https://docs.python.org/3", None),
     "rdflib": ("https://rdflib.readthedocs.io/en/stable/", None),
 }
 
