d8470b0adb5230907c96251268abe6be6d32a89c
Charles Tapley Hoyt <cthoyt@gmail.com>
2023-10-05 16:28:45 +0200

    Start

--- src/bioregistry/external/scicrunch/__init__.py
+++ src/bioregistry/external/scicrunch/__init__.py
@@ -7,4 +7,31 @@ https://docs.google.com/spreadsheets/d/1BEPZXZsENhK7592AR83xUwbPbR2J-GVQ/edit?\
 usp=sharing&ouid=107737386203376389514&rtpof=true&sd=true.
 """
 
+from pathlib import Path
+from typing import Mapping
+import csv
 
+HERE = Path(__file__).parent.resolve()
+PATH = HERE.joinpath("input.tsv")
+
+COLUMN_RENAMES = {"Resource_Name": "name"}
+
+
+def get_rrid(force_download: bool = False) -> Mapping[str, Mapping[str, str]]:
+    """Get RRIDs."""
+    rv = {}
+    with PATH.open() as file:
+        reader = csv.DictReader(file, delimiter="\t")
+        for record in reader:
+            prefix = record["prefix"]
+            name = record["Resource_Name"]
+            citations = record["Defining_Citation"]
+            rv[prefix] = {
+                "name": name,
+                "citations": citations,
+            }
+    return rv
+
+
+if __name__ == "__main__":
+    print(len(get_rrid(force_download=True)))  # noqa:T201
