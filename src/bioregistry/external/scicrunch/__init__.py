26ccfdd8c5c823e4570bd44e6cf63eb21acb054e
Charles Tapley Hoyt <cthoyt@gmail.com>
2023-10-05 17:29:47 +0200

    Add obvious RRIDs

--- src/bioregistry/external/scicrunch/__init__.py
+++ src/bioregistry/external/scicrunch/__init__.py
@@ -7,14 +7,19 @@ https://docs.google.com/spreadsheets/d/1BEPZXZsENhK7592AR83xUwbPbR2J-GVQ/edit?\
 usp=sharing&ouid=107737386203376389514&rtpof=true&sd=true.
 """
 
+import csv
 from pathlib import Path
 from typing import Mapping
-import csv
+
+__all__ = [
+    "get_rrid",
+]
 
 HERE = Path(__file__).parent.resolve()
 PATH = HERE.joinpath("input.tsv")
 
 COLUMN_RENAMES = {"Resource_Name": "name"}
+skip = {"RIN", "Resource Information Network"}
 
 
 def get_rrid(force_download: bool = False) -> Mapping[str, Mapping[str, str]]:
@@ -23,14 +28,43 @@ def get_rrid(force_download: bool = False) -> Mapping[str, Mapping[str, str]]:
     with PATH.open() as file:
         reader = csv.DictReader(file, delimiter="\t")
         for record in reader:
-            prefix = record["prefix"]
-            name = record["Resource_Name"]
-            citations = record["Defining_Citation"]
-            rv[prefix] = {
-                "name": name,
-                "citations": citations,
+            rrid_pattern = record.get("RRID_Identifier_Pattern")
+            if not rrid_pattern or not rrid_pattern.startswith("RRID:"):
+                continue
+
+            ddd = {
+                "name": record["Resource_Name"],
+                "homepage": record["Resource_URL"],
             }
+
+            prefix = rrid_pattern[len("RRID:") :].rstrip("_")
+            pubmeds = [
+                x[len("PMID:") :]
+                for x in _split(record["Defining_Citation"])
+                if x.startswith("PMID:")
+            ]
+            if pubmeds:
+                ddd["pubmeds"] = pubmeds
+
+            keywords = sorted(set(_split(record["Keywords"])).difference(skip))
+            if keywords:
+                ddd["keywords"] = keywords
+
+            abbreviation = record["Abbreviation"]
+            if abbreviation:
+                ddd["abbreviation"] = abbreviation
+            twitter = record["Twitter_Handle"]
+            if twitter:
+                ddd["twitter"] = twitter
+
+            # could get license
+            rv[prefix] = ddd
+
     return rv
+
+
+def _split(l: str):
+    return [c.strip() for c in l.split(",")]
 
 
 if __name__ == "__main__":
