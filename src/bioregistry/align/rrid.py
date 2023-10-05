10d9b7485a7eaf6bb1ecd2a8424ceeea3dfd7abd
Charles Tapley Hoyt <cthoyt@gmail.com>
2023-10-05 16:28:52 +0200

    Create rrid.py

---
+++ src/bioregistry/align/rrid.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+"""Align RRID with the Bioregistry."""
+
+from bioregistry.align.utils import Aligner
+from bioregistry.external.scicrunch import get_rrid
+
+__all__ = [
+    "RRIDAligner",
+]
+
+
+class RRIDAligner(Aligner):
+    """Aligner for the RRID."""
+
+    key = "rrid"
+    getter = get_rrid
+
+
+if __name__ == "__main__":
+    RRIDAligner.cli()
