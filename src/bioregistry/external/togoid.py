6d59cf4226031df127c28df7a65b64d7dfe31b33
Charles Tapley Hoyt <cthoyt@gmail.com>
2024-01-10 00:44:01 +0100

    Add initial data

--- src/bioregistry/external/togoid.py
+++ src/bioregistry/external/togoid.py
@@ -1,16 +1,32 @@
+# -*- coding: utf-8 -*-
+
+"""Download TogoID."""
+
 import yaml
+import json
+
 import requests
+
+from bioregistry.constants import EXTERNAL, URI_FORMAT_KEY
 
 __all__ = [
     "get_togoid",
 ]
 
+DIRECTORY = EXTERNAL / "togoid"
+DIRECTORY.mkdir(exist_ok=True, parents=True)
+RAW_PATH = DIRECTORY / "raw.json"
+PROCESSED_PATH = DIRECTORY / "processed.json"
 URL = (
     "https://raw.githubusercontent.com/togoid/togoid-converter/develop/swagger/swagger-config.yaml"
 )
 
 
-def get_togoid():
+def get_togoid(*, force_refresh: bool = False):
+    """Get the TogoID data."""
+    if PROCESSED_PATH.exists() and not force_refresh:
+        with PROCESSED_PATH.open() as file:
+            return json.load(file)
     rv = {}
     data = yaml.safe_load(requests.get(URL).text)
     subdata = data["paths"]["/config/dataset"]["get"]["responses"][200]["schema"]["properties"]
@@ -20,7 +36,7 @@ def get_togoid():
             "prefix": prefix,
             "name": dd["label"],
             "pattern": dd["regex"].replace("<id>", ""),
-            "uri_format": dd["prefix"] + "$1",  # this is right, they named it weird
+            URI_FORMAT_KEY: dd["prefix"] + "$1",  # this is right, they named it weird
         }
         examples_lists = dd.get("examples", [])
         if examples_lists:
@@ -29,8 +45,10 @@ def get_togoid():
         if category:
             rr["keywords"] = [category]
         rv[prefix] = rr
+    with PROCESSED_PATH.open("w") as file:
+        json.dump(rv, file, indent=2, sort_keys=True)
     return rv
 
 
 if __name__ == "__main__":
-    import pprint; pprint.pprint(get_togoid())
+    get_togoid(force_refresh=True)
