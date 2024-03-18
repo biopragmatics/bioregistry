9e8b19f673d646e52cb34ba3a30875d8e28ae8a0
Charles Tapley Hoyt <cthoyt@gmail.com>
2024-03-18 08:58:05 +0100

    Proactively update Prefix.cc
    References https://github.com/OBOFoundry/OBOFoundry.github.io/issues/1038

---
+++ src/bioregistry/export/prefixcc.py
@@ -0,0 +1,42 @@
+"""Update Prefix.cc to reflect the content of the Bioregistry.
+
+.. seealso:: https://github.com/OBOFoundry/OBOFoundry.github.io/issues/1038
+"""
+
+import requests
+import bioregistry
+
+
+def create(curie_prefix: str, uri_prefix: str) -> requests.Response:
+    """Send a CURIE prefix/URI prefix to the Prefix.cc "create" endpoint."""
+    return requests.post(
+        f"https://prefix.cc/{curie_prefix}",
+        data={"create": uri_prefix},
+    )
+
+
+def main():
+    """Add an OBO Foundry prefix to Prefix.cc."""
+    prefix_cc_map = requests.get("https://prefix.cc/context").json()["@context"]
+    for record in bioregistry.resources():
+        if not record.get_obofoundry_prefix():
+            continue
+        uri_prefix = record.get_uri_prefix()
+        if not uri_prefix:
+            continue
+        if uri_prefix == prefix_cc_map.get(record.prefix):
+            # No need to re-create something that's already
+            # both available and correct wrt Bioregistry/OBO
+            continue
+
+        print("Attempting to create a record for", record.prefix, uri_prefix)
+        res = create(record.prefix, uri_prefix)
+        print(res.text)
+
+        # We're breaking here since we can only make one
+        # update per day
+        break
+
+
+if __name__ == "__main__":
+    main()
