ada1293cbfe440948a59595646ed8e34d8888170
tanayshah2 <tanayshah960@gmail.com>
2024-08-06 20:08:41 -0700

    linted file

--- src/bioregistry/analysis/merge_and_rerank.py
+++ src/bioregistry/analysis/merge_and_rerank.py
@@ -1,5 +1,7 @@
 """Merges and reranks new/uncurated papers."""
+
 import sys
+
 import pandas as pd
 
 curated_file = sys.argv[1]
@@ -8,10 +10,10 @@ output_file = sys.argv[3]
 
 curated_df = pd.read_csv(curated_file)
 
-new_df = pd.read_csv(new_file, delimiter='\t', names=['pmid', 'title', 'score'])
+new_df = pd.read_csv(new_file, delimiter="\t", names=["pmid", "title", "score"])
 
 merged_df = pd.concat([curated_df, new_df], ignore_index=True)
 
-merged_df = merged_df.sort_values('score', ascending=False).drop_duplicates('pmid')
+merged_df = merged_df.sort_values("score", ascending=False).drop_duplicates("pmid")
 
 merged_df.to_csv(output_file, index=False)
