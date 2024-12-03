c87bb8c40963c607bc12d0677cc5a18904b8711b
Mufaddal Naguthanawala <m.naguthana@hotmail.com>
2024-12-02 22:41:19 -0500

    Mock pubmed_client.get_metadata_for_ids

--- tests/test_paper_ranking.py
+++ tests/test_paper_ranking.py
@@ -1,5 +1,6 @@
 """Test for checking the paper ranking model."""
 
+import json
 import datetime
 import unittest
 from pathlib import Path
@@ -18,15 +19,24 @@ class TestPaperRanking(unittest.TestCase):
         root_dir = root_dir = Path(__file__).resolve().parent.parent
         self.bioregistry_file = root_dir / "src" / "bioregistry" / "data" / "bioregistry.json"
         self.output_directory = root_dir / "exports" / "analyses" / "paper_ranking"
+        self.mock_data_path = root_dir / "src" / "bioregistry" / "data" / "mock_pubmed_data.json"
 
-        # Check if bioregistry file exists
+        # Check if bioregistry and mock data files exists
+        self.assertTrue(self.mock_data_path.exists(), "Mock data file does not exist")
         self.assertTrue(self.bioregistry_file.exists(), "Bioregistry file does not exist")
 
     @patch("pandas.DataFrame.to_csv")
-    def test_pipeline(self, mock_to_csv):
+    @patch("bioregistry.analysis.paper_ranking.pubmed_client.get_metadata_for_ids")
+    def test_pipeline(self, mock_to_csv, mock_get_metadata_for_ids):
         """Smoke test to ensure pipeline runs successfully without error."""
         start_date = datetime.date.today().isoformat()
         end_date = datetime.date.today().isoformat()
+
+        # Mock return value for get_metadata_for_ids
+        with open(self.mock_data_path, "r", encoding="utf-8") as file:
+            mock_data = json.load(file)
+
+        mock_get_metadata_for_ids.return_value = mock_data
 
         runner = CliRunner()
 
