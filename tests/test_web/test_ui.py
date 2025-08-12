d7f9cecb5a4311da5f8ac1272246bb839ebd03f1
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-08-12 13:00:20 +0200

    Update test_ui.py

--- tests/test_web/test_ui.py
+++ tests/test_web/test_ui.py
@@ -202,13 +202,24 @@ class TestUI(unittest.TestCase):
             # test some custom redirects
             for endpoint, expected in [
                 ("/EC:1.2.3.4", "https://www.enzyme-database.org/query.php?ec=1.2.3.4"),
+                ("/EC:2.3.1.n12", "https://www.enzyme-database.org/query.php?ec=2.3.1.n12"),
+                ("/EC:3.4.24.B15", "https://www.enzyme-database.org/query.php?ec=3.4.24.B15"),
                 ("/EC:1.2.3", "https://www.enzyme-database.org/class.php?c=1&sc=2&ssc=3"),
+                ("/EC:1.2.3.-", "https://www.enzyme-database.org/class.php?c=1&sc=2&ssc=3"),
                 ("/EC:1.2.3.-", "https://www.enzyme-database.org/class.php?c=1&sc=2&ssc=3"),
             ]:
                 with self.subTest(endpoint=endpoint):
                     res = client.get(endpoint, follow_redirects=False)
                     self.assertEqual(302, res.status_code)
                     self.assertEqual(expected, res.location)
+
+            # test some bad ones
+            for endpoint in [
+                "/EC:1.2.3.4.5",
+            ]:
+                with self.subTest(endpoint=endpoint):
+                    res = client.get(endpoint)
+                    self.assertEqual(404, res.status_code)
 
     def test_redirect_404(self):
         """Test 404 errors."""
