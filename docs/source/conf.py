b38dfbe6381471f29b333aef78bef9df2a6de5ae
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-10-06 23:46:21 +0200

    Bump version: 0.12.43 → 0.12.44-dev

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = f"{date.today().year}, Charles Tapley Hoyt"
 author = "Charles Tapley Hoyt"
 
 # The full version, including alpha/beta/rc tags.
-release = "0.12.43"
+release = "0.12.44-dev"
 
 # The short X.Y version.
 parsed_version = re.match(
