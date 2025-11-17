344838e08f39dcd2a1a96c696072296d5036448f
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-11-17 18:27:06 +0100

    Update add_cessda.py

--- src/bioregistry/curation/add_cessda.py
+++ src/bioregistry/curation/add_cessda.py
@@ -4,26 +4,44 @@ Docs on https://api.tech.cessda.eu/
 """
 
 import json
-from typing import Any
+import textwrap
 
-import requests
-from tqdm import tqdm
-from tabulate import tabulate
 import click
-import bioregistry
 import pystow
+import requests
+from tabulate import tabulate
+from tqdm import tqdm
+
+import bioregistry
+from bioregistry.schema.struct import Organization
 
 BASE = "https://vocabularies.cessda.eu"
 MODULE = pystow.module("cessda")
 
+AGENCIES: dict[str, Organization] = {
+    "GESIS": Organization(
+        ror="018afyw53", name="GESIS - Leibniz-Institute for the Social Sciences"
+    ),
+    "DDI Alliance": Organization(ror="015em2733", name="Data Documentation Initiative Alliance"),
+    "CESSDA": Organization(
+        ror="02wg9xc72", name="Consortium of European Social Science Data Archives"
+    ),
+}
+NN = {"GESIS": "gesis", "DDI Alliance": "ddi", "CESSDA": "cessda"}
+LICENSES = {
+    "http://creativecommons.org/licenses/by/4.0/": "CC-BY-4.0",
+}
 
+
+@click.command()
 def main() -> None:
+    """Add CESSDA prefixes."""
     res_json = MODULE.ensure_json(url=f"{BASE}/v2/vocabularies-published", name="index.json")
 
     rows = []
-    for superkey, superdata in res_json.items():
-        for prefix, language_to_version_to_url in tqdm(superdata.items(), desc=superkey):
-            path = MODULE.join(name=f"{prefix}.json")
+    for agency, superdata in res_json.items():
+        for subprefix, language_to_version_to_url in tqdm(superdata.items(), desc=agency):
+            path = MODULE.join(name=f"{subprefix}.json")
             if path.is_file():
                 data = json.loads(path.read_text())
             else:
@@ -34,8 +52,11 @@ def main() -> None:
                 #     v = language_to_version_to_url["en(TL)"]
                 #     version, url_last = max(v.items())
                 else:
-                    tqdm.write(f"[{prefix}] no english, choose from {set(language_to_version_to_url)}")
+                    tqdm.write(
+                        f"[{subprefix}] no english, choose from {set(language_to_version_to_url)}"
+                    )
                     continue
+
                 res_inner = requests.get(f"{BASE}{url_last}", timeout=10)
                 res_inner.raise_for_status()
                 data = res_inner.json()
@@ -45,30 +66,44 @@ def main() -> None:
             description = data.get("definitionEn")
             version = data.get("versionEn")
 
-            version_dict = data['versions'][0]
-            license = version_dict.get('licenseLink')
-            notation = version_dict['notation']  # also same as prefix
+            version_dict = data["versions"][0]
+            notation = version_dict["notation"]  # also same as prefix
 
-            concept = version_dict['concepts'][0]
-            example = concept['notation']
+            concept = version_dict["concepts"][0]
+            example = concept["notation"]
 
             # uri format
             uri_format = f"https://vocabularies.cessda.eu/vocabulary/{notation}?lang=en#code_$1"
 
+            prefix = f"{NN[agency]}.{notation.lower()}"
             resource = bioregistry.Resource(
-                prefix=notation,
+                prefix=prefix,
                 name=name,
                 example=example,
                 description=description,
-                license=license,
+                license=LICENSES[version_dict["licenseLink"]],
                 version=version,
                 uri_format=uri_format,
+                owners=[AGENCIES[agency]],
             )
-            
-            rows.append((superkey, notation, name, example, description, version, license, uri_format))
+            #bioregistry.add_resource(resource)
+            #bioregistry.manager.write_registry()
+
+            rows.append(
+                (
+                    agency,
+                    notation,
+                    name,
+                    example,
+                    textwrap.shorten(description, 60),
+                    version,
+                    license,
+                    uri_format,
+                )
+            )
 
     click.echo(tabulate(rows))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
