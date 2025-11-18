c94c7cebc9ed751763599ac2f72817675fd97799
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-11-18 23:40:32 +0100

    Add examples

--- src/bioregistry/curation/add_examples_from_ols.py
+++ src/bioregistry/curation/add_examples_from_ols.py
@@ -14,7 +14,7 @@ from bioregistry.external.ols.tib import TIB_OLS_BASE_URL
 
 
 def _get_example(prefix: str, *, size: int = 1000, base_url: str) -> str:
-    url = f"{base_url}/ontologies/{prefix}/terms?page=1&size={size}"
+    url = f"{base_url}/ontologies/{prefix}/terms?page=0&size={size}"
     return _get_example_helper(prefix, url)
 
 
@@ -24,16 +24,32 @@ def _get_example_helper(prefix: str, url: str) -> str:
         raise KeyError
     tqdm.write(f"[{prefix}] getting {url}")
     res = requests.get(url, timeout=15).json()
+
+    ppp = prefix.casefold() + "_"
+
     embedded = res.get("_embedded")
-    if not embedded:
-        tqdm.write(f"[{prefix} failure")
-        tqdm.write(str(res))
+    if embedded:
+        for term in embedded["terms"]:
+            iri = term['iri']
+            bioregistry.parse_iri(iri)
+
+
+            short_form = term["short_form"]
+            if short_form.casefold().startswith(ppp):
+                return cast(str, short_form[len(ppp):])
+            #else:
+            #    tqdm.write(f"[{prefix}] unable to use {short_form}\n\t{term}")
+        next_url = res["_links"]["next"]["href"]
+        return _get_example_helper(prefix, next_url)
+
+    page = res.get("page")
+    if not page:
         raise KeyError
-    for term in embedded["terms"]:
-        if term["short_form"].startswith(prefix.upper()):
-            return cast(str, term["short_form"])
-    next_url = res["_links"]["next"]["href"]
-    return _get_example_helper(prefix, next_url)
+
+    if page['totalElements'] == 0:
+        tqdm.write(f"no terms for {prefix}")
+        return None
+    raise KeyError
 
 
 SKIP = {"afo", "dicom", "ensemblglossary", "co_321:root", "co_336", "co_359", "gexo", "hcao"}
@@ -55,23 +71,21 @@ def _get_missing_ols(metaprefix: str) -> dict[str, str]:
 def _main_helper(metaprefix: str, base_url: str) -> None:
     """Get OLS examples."""
     r = dict(bioregistry.read_registry())
-    x = sorted(_get_missing_ols(metaprefix).items())
-    random.shuffle(x)
-    for prefix, ols_prefix in tqdm(x[:30]):
+    for prefix, ols_prefix in sorted(_get_missing_ols(metaprefix).items()):
         try:
-            res = _get_example(ols_prefix, size=1000, base_url=base_url)
+            example = _get_example(ols_prefix, size=1000, base_url=base_url)
         except KeyError:
             continue
-        if res is None:
+        if example is None:
             tqdm.write(f"no own terms in {prefix} (ols={ols_prefix})")
         else:
-            tqdm.write(f"[{prefix}] got example {res}")
-            if res.startswith(prefix.upper() + "_"):
-                res = res[len(prefix) + 1 :]
-            if res.startswith(prefix.upper() + ":"):
-                res = res[len(prefix) + 1 :]
-            r[prefix]["example"] = res
-    bioregistry.write_registry(r)
+            tqdm.write(f"[{prefix}] got example {example}")
+            if example.startswith(prefix.upper() + "_"):
+                example = example[len(prefix) + 1:]
+            if example.startswith(prefix.upper() + ":"):
+                example = example[len(prefix) + 1:]
+            r[prefix]["example"] = example
+            bioregistry.write_registry(r)
 
 
 def main() -> None:
