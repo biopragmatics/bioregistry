6f74e9aa26b79d5ace18524b4ee6eda642028a67
Charles Tapley Hoyt <cthoyt@gmail.com>
2026-03-19 12:32:01 +0100

    Update conf

--- src/bioregistry/external/wikidata/__init__.py
+++ src/bioregistry/external/wikidata/__init__.py
@@ -21,8 +21,9 @@ __all__ = [
 
 logger = logging.getLogger(__name__)
 
-DIRECTORY = Path(__file__).parent.resolve()
-PROCESSED_PATH = DIRECTORY / "processed.json"
+HERE = Path(__file__).parent.resolve()
+PROCESSED_PATH = HERE / "processed.json"
+CONFIG_PATH = HERE / "config.json"
 
 PROPERTIES_QUERY = dedent(
     """\
@@ -89,54 +90,9 @@ QUERY_FMT = dedent(
     """
 )
 
-SKIP = {
-    "P3205": "is a relationship",
-    "P3781": "is a relationship",
-    "P4545": "is a relationship",
-    "P3190": "is a relationship",
-    "P4954": "is a relationship",
-    "P4000": "is a relationship",
-    "P3189": "is a relationship",
-    "P3310": "is a relationship",
-    "P3395": "is a data property",
-    "P3387": "is a data property",
-    "P3337": "is a data property",
-    "P3485": "is a data property",
-    "P3486": "is a data property",
-    "P10322": "is a data property",
-    "P10630": "is a data property",
-    "P1193": "is a data property",
-    "P1603": "is a data property",
-    "P2067": "is a data property",
-    "P2844": "is a data property",
-    "P2854": "is a data property",
-    "P3487": "is a data property",
-    "P3492": "is a data property",
-    "P4214": "is a data property",
-    "P3488": "is a data property",
-    "P4250": "is a data property",
-    "P574": "is a data property",
-    "P7770": "is a data property",
-    "P783": "is a data property",
-    "P7862": "is a data property",
-    "P8010": "is a data property",
-    "P8011": "is a data property",
-    "P8049": "is a data property",
-    "P8556": "is a data property",
-    "P9107": "is a data property",
-    "Q112586709": "should not be annotated like a property",
-    "Q111831044": "should not be annotated like a property",
-    "Q115916376": "should not be annotated like a property",
-    "P1104": "is a data property",
-    "P10676": "is a data property",
-    "P181": "is a data property",
-    "P1843": "is a data property",
-    "P225": "is a data property",
-    "P3752": "is a data property",
-    "P8558": "is a data property",
-    "P6507": "is a data property",
-    "P428": "is a data property",
-}
+CONFIG = json.loads(CONFIG_PATH.read_text(encoding="utf-8"))
+SKIP = CONFIG["skips"]
+
 RENAMES = {"propLabel": "name", "propDescription": "description"}
 CANONICAL_DATABASES = {
     "P6800": "Q87630124",  # -> NCBI Genome
