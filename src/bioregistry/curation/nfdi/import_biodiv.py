063ab6c51eaabaaf85ed3b825108ae7512f999bf
Charles Tapley Hoyt <cthoyt@gmail.com>
2026-04-13 16:53:09 +0200

    Reorg

---
+++ src/bioregistry/curation/nfdi/import_biodiv.py
@@ -0,0 +1,35 @@
+from ontoportal_client import BioDivPortal
+import click
+from pathlib import Path
+import json
+from tabulate import tabulate
+
+import bioregistry
+
+COLLECTION_IDENTIFIER = "0000040"
+
+
+@click.command()
+def import_biodiv() -> None:
+    """Import biodiversity."""
+    path = Path(__file__).parent.joinpath("data.json")
+    if not path.is_file():
+        client = BioDivPortal()
+        ontologies = client.get_ontologies()
+        path.write_text(json.dumps(ontologies, indent=2))
+    else:
+        ontologies = json.loads(path.read_text())
+
+    rows = []
+    for ontology in ontologies:
+        name = ontology["name"]
+        acronym = ontology["acronym"]
+        if norm_id := bioregistry.normalize_prefix(acronym):
+            bioregistry.add_to_collection(COLLECTION_IDENTIFIER, norm_id)
+        else:
+            rows.append((acronym, name, f"https://biodivportal.gfbio.org/ontologies/{acronym}"))
+    click.echo(tabulate(rows, headers="keys"))
+
+
+if __name__ == '__main__':
+    import_biodiv()
