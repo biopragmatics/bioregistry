81ae590234b4d248baa453e74fb150c382789c99
Charles Tapley Hoyt <cthoyt@gmail.com>
2026-04-14 23:17:14 +0200

    Refactor NFDI UI

--- src/bioregistry/curation/nfdi/sync.py
+++ src/bioregistry/curation/nfdi/sync.py
@@ -1,80 +1,17 @@
 """Import TIB Terminology Service collections into the corresponding Bioregistry collections."""
 
-from collections import Counter
-
 import click
-from tabulate import tabulate
-from tqdm import tqdm
 
-import bioregistry
-from bioregistry.external.bartoc import get_bartoc, get_bartoc_registries
-from bioregistry.external.ols.tib import get_tib_ts
-from bioregistry.schema_utils import get_collection_mappings
-
-# Add additional mappings in collections.json's `mappings` field, e.g., with
-# `tib.collection` as a prefix for TIB OLS collections
-KEYWORD_TO_COLLECTION = {v: k for k, v in get_collection_mappings("tib.collection").items()}
-
-
-def _import_tib() -> None:
-    tqdm.write("\n\nImporting Collections from TIB OLS\n\n")
-    counter: Counter[str] = Counter()
-
-    tib_to_internal = bioregistry.get_registry_invmap("tib")
-
-    for tib_prefix, tib_data in get_tib_ts().items():
-        internal_prefix = tib_to_internal.get(tib_prefix)
-        if not internal_prefix:
-            tqdm.write(f"no mapping from {tib_prefix}")
-            continue
-        for keyword in tib_data.get("keywords", []):
-            collection_id = KEYWORD_TO_COLLECTION.get(keyword.lower())
-            if not collection_id:
-                counter[keyword.lower()] += 1
-                continue
-            bioregistry.add_to_collection(collection_id, internal_prefix)
-
-    bioregistry.manager.write_collections()
-    tqdm.write(tabulate(counter.most_common(), headers=["unmapped TIB keyword", "count"]))
-
-
-def _import_bartoc() -> None:
-    rows = []
-    bartoc_registries = get_bartoc_registries()
-    bartoc_to_internal = bioregistry.get_registry_invmap("bartoc")
-    bartoc_data = get_bartoc()
-
-    for collection_id, registry_bartoc_id in get_collection_mappings("bartoc").items():
-        for resource_bartoc_id in bartoc_registries[registry_bartoc_id]:
-            prefix = bartoc_to_internal.get(resource_bartoc_id)
-            if prefix:
-                bioregistry.add_to_collection(collection_id, prefix)
-            else:
-                rows.append(
-                    (
-                        resource_bartoc_id,
-                        f"https://bartoc.org/node/{resource_bartoc_id}",
-                        bartoc_data[resource_bartoc_id].get("short_names"),
-                        bartoc_data[resource_bartoc_id].get("name"),
-                        bartoc_data[resource_bartoc_id].get("homepage"),
-                        bartoc_data[resource_bartoc_id].get("uri_format"),
-                    )
-                )
-    bioregistry.manager.write_collections()
-    tqdm.write(
-        tabulate(
-            rows,
-            headers=["bartoc", "bartoc link", "prefix", "name", "homepage", "uri_format"],
-            showindex=False,
-        )
-    )
+from bioregistry.curation.nfdi.import_bartoc import import_bartoc
+from bioregistry.curation.nfdi.import_tib import import_tib
 
 
 @click.command()
-def main() -> None:
+@click.pass_context
+def main(ctx: click.Context) -> None:
     """Populate collections based on keywords from the TIB terminology service."""
-    _import_bartoc()
-    _import_tib()
+    ctx.invoke(import_bartoc)
+    ctx.invoke(import_tib)
 
 
 if __name__ == "__main__":
