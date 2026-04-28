6fdbffde662e9980b4c4c8de12e6d630a2d22c96
Charles Tapley Hoyt <cthoyt@gmail.com>
2026-04-28 12:02:52 +0200

    Create add_owner.py

---
+++ src/bioregistry/curation/add_owner.py
@@ -0,0 +1,38 @@
+"""Add owner."""
+
+import bioregistry
+import ror_downloader
+
+from urllib.parse import urlparse
+
+from tabulate import tabulate
+
+
+def main():
+    domain_to_organization = {
+        domain: organization
+        for organization in ror_downloader.get_organizations()[1]
+        for domain in organization.domains
+    }
+    rows = []
+    for resource in bioregistry.resources():
+        if resource.get_owners():
+            continue
+        homepage = resource.get_homepage()
+        if "www.w3.org" in homepage:
+            continue
+
+        domain = urlparse(homepage).netloc
+        if not domain:
+            continue
+
+        if org := domain_to_organization.get(domain):
+            rows.append((resource.prefix, domain, org.id, org.get_preferred_label()))
+        elif org := domain_to_organization.get(domain.removeprefix("www.")):
+            rows.append((resource.prefix, domain, org.id, org.get_preferred_label()))
+
+
+    print(tabulate(rows, tablefmt="github"))
+
+if __name__ == '__main__':
+    main()
