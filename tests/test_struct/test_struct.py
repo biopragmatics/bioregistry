5ab9ca56d210cd63318beaf23bce2554e62ee4cc
Charles Tapley Hoyt <cthoyt@gmail.com>
2026-06-12 09:40:43 +0200

    Update test_struct.py

--- tests/test_struct/test_struct.py
+++ tests/test_struct/test_struct.py
@@ -3,6 +3,7 @@
 import unittest
 
 from bioregistry import Resource
+from bioregistry.schema.record_accumulator import get_converter
 
 
 class StructTest(unittest.TestCase):
@@ -21,3 +22,21 @@ class StructTest(unittest.TestCase):
 
         resource = Resource(prefix="test", uri_format="https://example.com/$1.html")
         self.assertEqual("https://bioregistry.io/test:", resource.get_uri_prefix())
+
+    def test_record_accumulator(self) -> None:
+        """Test record accumulator."""
+        resource = Resource(prefix="test")
+
+        converter = get_converter([resource])
+        self.assertEqual({}, converter.bimap)
+
+        converter = get_converter([resource], stubs=True)
+        self.assertEqual({"test": "https://bioregistry.io/test:"}, converter.bimap)
+
+        resource = Resource(prefix="test", uri_format="https://example.com/$1")
+        converter = get_converter([resource])
+        self.assertEqual({"test": "https://example.com/"}, converter.bimap)
+
+        resource = Resource(prefix="test", uri_format="https://example.com/$1.html")
+        converter = get_converter([resource])
+        self.assertEqual({"test": "https://bioregistry.io/test:"}, converter.bimap)
