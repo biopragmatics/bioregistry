3730c1c28fbf8b7b9d0c87bc941b930cc103ee9d
Charles Tapley Hoyt <cthoyt@gmail.com>
2026-09-09 00:37:34 +0200

    Update test_data.py

--- tests/test_data.py
+++ tests/test_data.py
@@ -67,12 +67,15 @@ class TestRegistry(unittest.TestCase):
             https://github.com/biopragmatics/bioregistry/issues/180
         """
         text = BIOREGISTRY_PATH.read_text(encoding="utf8")
-        linted_text = json.dumps(
-            json.loads(text),
-            indent=2,
-            sort_keys=True,
-            ensure_ascii=False,
-        ) + "\n"
+        linted_text = (
+            json.dumps(
+                json.loads(text),
+                indent=2,
+                sort_keys=True,
+                ensure_ascii=False,
+            )
+            + "\n"
+        )
         self.assertEqual(
             linted_text,
             text,
