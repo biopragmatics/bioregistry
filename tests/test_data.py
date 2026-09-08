ff510b12cf78fdb2fc079803533ac50cb36e93bf
Charles Tapley Hoyt <cthoyt@gmail.com>
2026-09-08 23:56:32 +0200

    Implement SKOSMOS ingestion and add initial curations (#1785)

--- tests/test_data.py
+++ tests/test_data.py
@@ -26,6 +26,7 @@ from bioregistry.constants import (
     EMAIL_RE,
     METAREGISTRY_PATH,
 )
+from bioregistry.curation.add_skosmos import SKOSMOS_APIS
 from bioregistry.export.rdf_export import resource_to_rdf_str
 from bioregistry.external import GETTERS
 from bioregistry.license_standardizer import REVERSE_LICENSES, standardize_license
@@ -1307,6 +1308,12 @@ class TestRegistry(unittest.TestCase):
         """Test status contributions."""
         status_contributions = read_status_contributions(self.registry)
         self.assertIn("0009-0006-4842-7427", status_contributions)
+
+    def test_skosmos(self) -> None:
+        """Check all SKOSMOS keys are valid Bioregistry prefixes."""
+        for key, _ in SKOSMOS_APIS:
+            with self.subTest(prefix=key):
+                self.assertIn(key, self.registry)
 
     def test_download_obo(self) -> None:
         """Test getting OBO download link."""
