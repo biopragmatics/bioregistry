a3e0e741216697c0577af0c63dba462c546ff76d
Charles Tapley Hoyt <cthoyt@gmail.com>
2026-09-17 09:41:53 +0200

    Improve resource bibliometrics display (#2090)

--- src/bioregistry/app/components/resource.py
+++ src/bioregistry/app/components/resource.py
@@ -41,6 +41,25 @@ def resource(prefix: str) -> str | werkzeug.Response | tuple[str, int]:
         _resource.get_curie(example_extra, use_preferred=True) for example_extra in example_extras
     ]
     name_pack = manager._repack(_resource.get_name(provenance=True))
+
+    contacts = []
+    if contact := _resource.get_contact():
+        contacts.append(contact)
+        if _resource.contact_extras:
+            contacts.extend(_resource.contact_extras)
+
+    reviewers = []
+    if review := _resource.reviewer:
+        reviewers.append(review)
+        if _resource.reviewer_extras:
+            reviewers.extend(_resource.reviewer_extras)
+
+    contributors = []
+    if contributor := _resource.contributor:
+        contributors.append(("creator", contributor))
+    if _resource.contributor_extras:
+        contributors.extend(("contributor", c) for c in _resource.contributor_extras)
+
     return render_template(
         "resource.html",
         zip=zip,
@@ -79,7 +98,9 @@ def resource(prefix: str) -> str | werkzeug.Response | tuple[str, int]:
         jskos_download=_resource.get_download_jskos(),
         namespace_in_lui=_resource.get_namespace_in_lui(),
         deprecated=manager.is_deprecated(prefix),
-        contact=_resource.get_contact(),
+        contacts=contacts,
+        reviewers=reviewers,
+        contributors=contributors,
         banana=_resource.get_banana(),
         description=manager.get_description(prefix, use_markdown=True),
         appears_in=manager.get_appears_in(prefix),
